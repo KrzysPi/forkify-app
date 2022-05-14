6803f8d01fffe99c6cef900ebe400ff5b8a04f26
KrzysPi <krzyspioin@gmail.com>
2022-05-14 22:20:09 +0200

    config.js update

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,3 @@
-import { isInteger } from 'core-js/core/number';
-
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
