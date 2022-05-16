9862d9c80346c53623a9a8e4bfe334494d4274c6
KrzysPi <krzyspioin@gmail.com>
2022-05-16 10:30:52 +0200

    Number of pages

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -19,8 +19,8 @@ class PaginationView extends View {
         </svg>
     </button>`;
     // do powyższego HTML dodliśmy `data-goto="${curPage + 1}" żeby muc to zczytać do zmiennej goToPage (linia 54)
+
     const btnPrev = `
-    <div>${numPages}</div>
     <button data-goto="${
       curPage - 1
     }" class="btn--inline pagination__btn--prev">
@@ -30,18 +30,21 @@ class PaginationView extends View {
       <span>Page ${curPage - 1}</span>
     </button>`;
     // console.log(numPages);
+
+    const numPagesView = `<span class="num_pages pagination__btn"> ${numPages} pages</span>`;
+
     // Page 1 there are other pages
     if (curPage === 1 && numPages > 1) {
-      return btnNext;
+      return btnNext + numPagesView;
     }
 
     //  Last page
     if (curPage === numPages && numPages > 1) {
-      return btnPrev;
+      return btnPrev + numPagesView;
     }
     // Other page
     if (curPage < numPages) {
-      return btnPrev + btnNext;
+      return btnPrev + btnNext + numPagesView;
     }
     // Page one there are NO other pages
     return ``;
